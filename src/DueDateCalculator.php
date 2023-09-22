91779b49f6b9579e3319ff5cdcbb904b8a805314
Zoltan Borsos <zolli07@gmail.com>
2023-09-22 22:28:40 +0200

    feat: implemented input checks for DueDateCalculator::calculateDueDate() method

--- src/DueDateCalculator.php
+++ src/DueDateCalculator.php
@@ -7,6 +7,7 @@ namespace Zolli\Emarsys\Homework;
 use \DateTimeImmutable;
 use \DateInterval;
 use Zolli\Emarsys\Homework\Contracts\DueDateCalculatorInterface;
+use Zolli\Emarsys\Homework\Exception\IssueException;
 use Zolli\Emarsys\Homework\Model\WorkTerms;
 
 class DueDateCalculator implements DueDateCalculatorInterface
@@ -15,6 +16,9 @@ class DueDateCalculator implements DueDateCalculatorInterface
 
     public function calculateDueDate(DateTimeImmutable $submitDateTime, int $turnaroundTimeHours): DateTimeImmutable
     {
+        $this->assertSubmitDateIsValid($submitDateTime);
+        $this->assertTurnaroundTimeIsValid($turnaroundTimeHours);
+
         $dueDateTime = clone $submitDateTime;
 
         while ($turnaroundTimeHours > 0) {
@@ -47,5 +51,25 @@ class DueDateCalculator implements DueDateCalculatorInterface
         $dateTimeDayName = $dateTime->format('l');
 
         return in_array($dateTimeDayName, $this->workTerms->getWorkingDays(), true);
+    }
+
+    /**
+     * @throws IssueException
+     */
+    private function assertSubmitDateIsValid(DateTimeImmutable $dateTime): void
+    {
+        if (!$this->isDateInsideWorkingHours($dateTime)) {
+            throw IssueException::cannotSubmitOutsideWorkHours();
+        }
+    }
+
+    /**
+     * @throws IssueException
+     */
+    private function assertTurnaroundTimeIsValid(int $turnaroundTime): void
+    {
+        if ($turnaroundTime <= 0) {
+            throw IssueException::turnaroundTimeMustBePositive();
+        }
     }
 }
--- src/Exception/IssueException.php
+++ src/Exception/IssueException.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Zolli\Emarsys\Homework\Exception;
 
-class IssueException
-{
+use \Exception;
 
+class IssueException extends Exception
+{
+    public static function cannotSubmitOutsideWorkHours(): self
+    {
+        return new self('Cannot submit issue outside work hours!');
+    }
+
+    public static function turnaroundTimeMustBePositive(): self
+    {
+        return new self('Turnaround time must be a positive integer!');
+    }
 }
