e80d0b593349288659fcc5f2b01b7e4292e69117
Zoltan Borsos <zolli07@gmail.com>
2023-09-22 21:12:53 +0200

    feat: Introduced WorkTerm class to represent working days and hours and created test for expected working

---
+++ src/Exception/WorkTermsException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Zolli\Emarsys\Homework\Exception;
+
+class WorkTermsException
+{
+
+}
---
+++ tests/Unit/Model/WorkTermTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zolli\Emarsys\Homework\Tests\Unit\Model;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Zolli\Emarsys\Homework\Exception\WorkTermsException;
+use Zolli\Emarsys\Homework\Model\WorkTerms;
+
+class WorkTermTest extends TestCase
+{
+    public function testObjectPropertiesAndGetters(): void
+    {
+        $workdays = ['Monday', 'Tuesday'];
+        $workdayStart = 9;
+        $workdayEnd = 17;
+
+        $subject = new WorkTerms($workdays, $workdayStart, $workdayEnd);
+
+        $this->assertEquals($workdays, $subject->getWorkingDays());
+        $this->assertEquals($workdayStart, $subject->getWorkdayStartHour());
+        $this->assertEquals($workdayEnd, $subject->getWorkdayEndHour());
+    }
+
+    public function testItThrowsExceptionWhenWorkdaysNotProvided()
+    {
+        $this->expectException(WorkTermsException::class);
+        $this->expectExceptionMessage('At least one working day needs to be provided!');
+
+        new WorkTerms([], 9, 17);
+    }
+
+    #[DataProvider('invalidWorkingHourDataProvider')]
+    public function testItThrowsExceptionWhenWorkHoursIsLessThanOne(int $workHourStart, int $workHourEnd)
+    {
+        $this->expectException(WorkTermsException::class);
+        $this->expectExceptionMessage('At least one working day needs to be provided!');
+
+        new WorkTerms([], $workHourStart, $workHourEnd);
+    }
+
+    public static function invalidWorkingHourDataProvider(): array
+    {
+        return [
+            'work_hour_explicit_zero' => [9, 9],
+            'work_hour_negative' => [9, 5],
+        ];
+    }
+}
